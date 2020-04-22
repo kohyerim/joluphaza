d9d36a63925ddd8a5669982a8d82f21bd0787fe8
kohyerim <kohyerim78@gmail.com>
2020-04-22 17:49:23 +0900

    url 자체를 이동하는 방법찾아보기

--- src/Dreamy.java
+++ src/Dreamy.java
@@ -1,7 +1,17 @@
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.Base64;
 
 public class Dreamy {
@@ -9,8 +19,8 @@ public class Dreamy {
     private String id = null;
     private String pw = null;
     private String uri = "https://dreamy.jejunu.ac.kr";
-    private String body = "";
     private URL url = null;
+    private String line = null;
 
     public Dreamy(String id, String pw){
         this.id = this.encoder.encodeToString(id.getBytes());
@@ -18,16 +28,22 @@ public class Dreamy {
     }
 
 
-    public void login() throws IOException {
-        this.body = "/frame/sysUser.do?next=" + "&tmpu=" + this.id + "&tmpw=" + this.pw + "&app=null&z=Y&userid=&password=";
-        url = new URL(this.uri + this.body);
+    public HttpsURLConnection login() throws IOException {
+        String body = "/frame/sysUser.do?next=";
+        String param = "&tmpu=" + this.id + "&tmpw=" + this.pw + "&mobile=n&app=null&z=Y&userid=&password=";url = new URL(this.uri + body);
+        url = new URL(uri + body + param);
         HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
 
-        //System.out.println(conn);
-
+        return conn;
     }
 
-    public void getHakjuk(){
-        this.body = "/hjju/hj/sta_hj_1010q.jejunu";
+    public void getSooup(HttpsURLConnection conn) throws IOException {
+        String body = "/susj/su/sta_su_8331r.jejunu" ;
+        url = new URL(this.uri + body);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
+        while((line = br.readLine()) != null ){
+            System.out.println(line);
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,41 @@
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String id = "2017108249";
+    public static void main(String[] args){
+        String id = "id";
         String pw = "pw";
         Dreamy dreamy = new Dreamy(id, pw);
-        dreamy.login();
-        dreamy.getHakjuk();
+
+        try{
+            // SSLHandShakeException Fixed
+            TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            } };
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            // code start
+            HttpsURLConnection conn = dreamy.login();
+            dreamy.getSooup(conn);
+        }
+        catch (NoSuchAlgorithmException | IOException | KeyManagementException e) {
+            e.printStackTrace();
+        }
     }
 }
